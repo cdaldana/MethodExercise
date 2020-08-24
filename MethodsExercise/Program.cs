bf43a8c416af9eac836395afe1dda1e529a687ab
Christian Aldana <cdaldana90@gmail.com>
2020-08-24 13:25:11 -0500

    finished profile

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -23,12 +23,21 @@ namespace MethodsExercise
             var animal = Console.ReadLine();
 
 
-           Console.WriteLine($"Great! Now, what is your favorite band?");
+            Console.WriteLine($"Great! Now, what is your favorite band?");
             var band = Console.ReadLine();
 
 
-            Console.WriteLine($"I love {band}. My favorite song is fascination street!");
-            var song = Console.ReadLine();
+            Console.WriteLine("----------------------------------");
+
+            Console.WriteLine($"Thanks, {userName}! Here is your profile");
+
+            Console.WriteLine("----------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
+            
 
 
         }
