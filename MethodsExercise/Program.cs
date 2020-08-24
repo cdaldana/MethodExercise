6ae87d9c75fdcfd36cb07d8a43e3feab5b87c16a
Christian Aldana <cdaldana90@gmail.com>
2020-08-24 13:08:16 -0500

    began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -16,18 +16,18 @@ namespace MethodsExercise
             Console.WriteLine("Hello - What is your first name?");
             var userName = Console.ReadLine();
 
-            Console.WriteLine($"Hi, {userName}. - What is your favorite color?");
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
             var color = Console.ReadLine();
 
-            Console.WriteLine($"{color} is an awesome color! - What is your favorite animal?");
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
             var animal = Console.ReadLine();
 
 
-           Console.WriteLine($"Great! - Now, what is your favorite band?");
+           Console.WriteLine($"Great! Now, what is your favorite band?");
             var band = Console.ReadLine();
 
 
-            Console.WriteLine($"I love {band}. - My favorite song is fascination street!");
+            Console.WriteLine($"I love {band}. My favorite song is fascination street!");
             var song = Console.ReadLine();
 
 
